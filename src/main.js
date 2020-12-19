6fc7f670fc93bb175b9dcdabd59d0156651ee2b7
autumnfish <517729329@qq.com>
2020-12-19 21:46:07 +0800

    init

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 整合vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
+// 字体图标
+import './style/iconfont.css'
+
 
 Vue.config.productionTip = false
 
