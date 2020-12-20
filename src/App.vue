66481a0ad9a7152063d91fb46c6baedad0a70411
autumnfish <517729329@qq.com>
2020-12-20 19:38:39 +0800

    2020-12-20

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,42 @@
 <template>
-  <div>app</div>
+  <div>
+    <!-- <DefaultSlot>
+      <template>
+        <div class="out-box">
+          感觉自己萌萌哒!!!
+        </div>
+      </template>
+    </DefaultSlot> -->
+
+    <NameSlot>
+      <!-- <template v-slot:top> -->
+      <template #top>
+        中分还带波浪👱
+      </template>
+      感觉自己萌萌哒
+      <!-- <template v-slot:bottom> -->
+      萌你奶奶个腿
+      <!-- </template> -->
+    </NameSlot>
+  </div>
 </template>
 
 <script>
+// import DefaultSlot from './components/01.默认插槽'
+import NameSlot from './components/02.具名插槽'
 export default {
-
+  name: 'app',
+  components: {
+    // DefaultSlot
+    NameSlot
+  }
 }
 </script>
 
 <style>
-
+.out-box {
+  color: white;
+  background-color: skyblue;
+  padding: 20px;
+}
 </style>
---
+++ src/components/02.具名插槽.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="name-slot-container">
+    <h2>具名插槽测试</h2>
+    <div class="top">
+      <slot name="top"></slot>
+    </div>
+    <div class="mid">
+      <slot></slot>
+    </div>
+    <div class="bottom">
+      <slot name="bottom"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.name-slot-container {
+  background-color: skyblue;
+}
+.name-slot-container > div {
+  height: 200px;
+}
+.top {
+  background-color: purple;
+}
+.mid {
+  background-color: orange;
+}
+.bottom {
+  background-color: pink;
+}
+</style>
